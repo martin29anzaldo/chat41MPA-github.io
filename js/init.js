d8980bd68f69584ed31c7d597a1b7aa77d4dc6c6
martin29anzaldo <87622691+martin29anzaldo@users.noreply.github.com>
2021-07-18 18:52:57 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyC8apeWyQ8-rXfgQJplQaDQYQo8NpUqB1k",
+    authDomain: "chat41jach.firebaseapp.com",
+    projectId: "chat41jach",
+    storageBucket: "chat41jach.appspot.com",
+    messagingSenderId: "558413991965",
+    appId: "1:558413991965:web:3f28ed7c6e47b6d325b6f4",
+    measurementId: "G-MSPRMYVM7S"
+    });
